4597b995474bb1075dda0be26ceeadd1b0bd4d25
0x1eef <0x1eef@hardenedbsd.org>
2026-02-12 17:35:58 -0300

    Fix jail.{Attach,Remove}
    Neither function was working properly

--- jail_remove.go
+++ jail_remove.go
@@ -6,9 +6,9 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// Attach the current proccess to a jail
-func Attach(jid int32) error {
-	_, _, e1 := unix.Syscall(uintptr(sysJailAttach), uintptr(jid), 0, 0)
+// Removes a jail
+func Remove(jid int32) error {
+	_, _, e1 := unix.Syscall(uintptr(sysJailRemove), uintptr(jid), 0, 0)
 	if e1 != 0 {
 		switch int(e1) {
 		case ErrJailAttachUnprivilegedUser:
